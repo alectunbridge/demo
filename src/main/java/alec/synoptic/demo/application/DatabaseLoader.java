704ae6635cc17eda643c6f87b6b8a3ca9bd05c6d
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2020-03-03 17:05:31 +0000

    initial commit

---
+++ src/main/java/alec/synoptic/demo/application/DatabaseLoader.java
@@ -0,0 +1,23 @@
+package alec.synoptic.demo.application;
+
+import alec.synoptic.demo.employee.Employee;
+import alec.synoptic.demo.employee.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final EmployeeRepository repository;
+
+    @Autowired
+    public DatabaseLoader(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+    }
+}
